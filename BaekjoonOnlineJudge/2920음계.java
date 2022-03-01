7184a2f6a36c4a02d04f016ae623be6a88785710
khw.rlagusdn1996@gmail.com <khw.rlagusdn1996@gmail.com>
2022-03-01 20:08:15 +0900

    test2

--- BaekjoonOnlineJudge/2920음계.java
+++ BaekjoonOnlineJudge/2920음계.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-//test
+//test2
 public class Main {
 
     public static void main(String[] args) throws Exception {
