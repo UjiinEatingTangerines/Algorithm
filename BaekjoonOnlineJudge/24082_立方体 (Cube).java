a569ebe18321b860cc3e84d8096f9e86694cfaba
1996.devjiinu@gmail.com <1996.devjiinu@gmail.com>
2022-03-22 23:20:27 +0900

    백준 24082번 문제 - 브론즈 5

---
+++ BaekjoonOnlineJudge/24082_立方体 (Cube).java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        bw.write(String.valueOf(n*n*n));
+        bw.newLine();
+        bw.flush();
+
+    }
+}
