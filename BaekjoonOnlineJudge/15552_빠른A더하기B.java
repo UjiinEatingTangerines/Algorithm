ef6f78410cb028faa7c417d845dbfc7ee0b633b7
1996.devjiinu@gmail.com <1996.devjiinu@gmail.com>
2022-03-23 23:27:08 +0900

    백준 15552번 문제 - 브론즈 2

---
+++ BaekjoonOnlineJudge/15552_빠른A더하기B.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        for(int i=0; i<n; i++){
+
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write(String.valueOf(a+b));
+            bw.newLine();
+        }
+        bw.flush();
+
+    }
+}
