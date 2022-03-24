6c4e0e4021582e3be22f95aa065d911cee0ea676
1996.devjiinu@gmail.com <1996.devjiinu@gmail.com>
2022-03-24 22:46:36 +0900

    백준 24078번 문제 - 브론즈 5

---
+++ BaekjoonOnlineJudge/24078_余り(Remainder).java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int x = Integer.parseInt(br.readLine());
+
+        bw.write(String.valueOf(x%21));
+        bw.newLine();
+        bw.flush();
+    }
+
+}
