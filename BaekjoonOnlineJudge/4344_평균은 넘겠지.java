3b3032218531eea41117306da2dd1bdef519736d
1996.devjiinu@gmail.com <1996.devjiinu@gmail.com>
2022-04-20 19:45:19 +0900

    22.04.20 브론즈 1

---
+++ BaekjoonOnlineJudge/4344_평균은 넘겠지.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class test {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            int a = Integer.valueOf(st.nextToken());
+            double result = 0.000;
+
+            int[] arr = new int[a];
+            for(int j=0; j<a; j++){
+                int score =  Integer.valueOf(st.nextToken());
+                result += score;
+                arr[j] = score;
+            }
+            result = result/a;
+            int count =0;
+            for(int j=0; j<a; j++){
+                if(result < arr[j]) count++;
+            }
+            result = ((double) count/a)*100;
+            String result2 = String.format("%.3f", result);
+            bw.write(result2 + "%");
+            bw.newLine();
+        }
+        bw.flush();
+
+
+    }
+}
