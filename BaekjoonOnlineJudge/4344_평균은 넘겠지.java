edfd161af5991708f1d517a914fc8178a76be580
1996.devjiinu@gmail.com <1996.devjiinu@gmail.com>
2022-04-22 23:18:58 +0900

    update

--- BaekjoonOnlineJudge/4344_평균은 넘겠지.java
+++ BaekjoonOnlineJudge/4344_평균은 넘겠지.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class test {
+public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
