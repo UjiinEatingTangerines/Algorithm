268e3c11686dd14196288aabe8f956796536a555
1996.devjiinu@gmail.com <1996.devjiinu@gmail.com>
2022-04-25 21:49:30 +0900

    22.04.25 브론즈 3

---
+++ BaekjoonOnlineJudge/2441_별찍기4.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        String str = "*";
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=n; i>0; i--){
+            for(int j=0; j<n-i; j++){
+                sb.append(" ");
+            }
+            for(int j=0; j<i; j++){
+                sb.append(str);
+            }
+            bw.write(sb.toString());
+            bw.newLine();
+            sb.delete(0,sb.length());
+        }
+        bw.flush();
+
+    }
+}
