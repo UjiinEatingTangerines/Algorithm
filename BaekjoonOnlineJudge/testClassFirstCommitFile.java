6ca04f22e6f44a66a5b0f48eef96be3092a0a401
UjiinEatingTangerines <1996.devjiinu@gmail.com>
2022-08-21 00:48:02 -0700

    리팩토링

--- BaekjoonOnlineJudge/testClassFirstCommitFile.java
+++
@@ -1,7 +0,0 @@
-public class testClassFirstCommitFile {
-
-    public static void main(String[] args) {
-        System.out.println("this is test commit");
-        System.out.println("this is Two commit");
-    }
-}
